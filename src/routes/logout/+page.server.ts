452de1321906a2c1db4a5bbc783b6587d2b377c4
hansaskov <hansaskov1999@gmail.com>
2023-04-26 20:55:54 +0200

    added logout logic

---
+++ src/routes/logout/+page.server.ts
@@ -0,0 +1,13 @@
+import { redirect, type Actions } from "@sveltejs/kit";
+import { auth } from "$lib/server/lucia";
+
+export const actions: Actions = {
+	default: async ({ locals }) => {
+		const session = await locals.auth.validate();
+		if (!session) throw redirect(302, "/");
+
+		await auth.invalidateSession(session.sessionId); // invalidate session
+		locals.auth.setSession(null); // remove cookie
+		throw redirect(302, "/");
+	}
+};
