fa5d9c7c4ad0d3e8d4ca66d45da14414ce182fe1
hansaskov <hansaskov1999@gmail.com>
2023-05-06 21:51:32 +0200

    refactior routing

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,10 +5,8 @@ import { redirectToLogin } from '$lib/server/redirects';
 export const load: PageServerLoad = async ({ locals, url }) => {
 	const { user } = await locals.auth.validateUser();
 	if (!user) throw redirect(302, redirectToLogin(url));
-	if (user.userInfoSet == false) throw redirect(302, '/signup/setup');
-	if (user.emailVerified == false) {
-		throw redirect(302, '/email/verification');
-	}
+	if (!user.userInfoSet) throw redirect(302, '/signup/setup');
+	if (!user.emailVerified) throw redirect(302, '/email/verification');
 
 	return {
 		user
