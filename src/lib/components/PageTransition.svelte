5683488d39d62f48a8cabaac93b98103cb778960
hansaskov <hansaskov1999@gmail.com>
2023-05-08 13:59:53 +0200

    test

--- src/lib/components/PageTransition.svelte
+++ src/lib/components/PageTransition.svelte
@@ -2,17 +2,6 @@
 	import { fly, type FlyParams } from 'svelte/transition';
 	import { cubicIn, cubicOut } from 'svelte/easing';
 
-	import { beforeNavigate, afterNavigate } from '$app/navigation';
-
-	let isLoading = false;
-	beforeNavigate(({ to }) => {
-		if (to?.route.id) {
-			isLoading = true;
-		}
-	});
-
-	afterNavigate(() => (isLoading = false));
-
 	const duration = 250;
 	const delay = duration + 33;
 	const y = 8;
