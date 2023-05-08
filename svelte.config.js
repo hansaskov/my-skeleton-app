c741cc1dc5f8513621c94373e139440d7f6d261b
hansaskov <hansaskov1999@gmail.com>
2023-05-08 14:25:53 +0200

    change back to edge

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,7 @@ const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	runtime: 'nodejs18.x',
+	runtime: 'edge',
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
