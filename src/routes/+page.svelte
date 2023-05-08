5bc4407439786b7c0a6e0384d55c163fa24a3abb
hansaskov <hansaskov1999@gmail.com>
2023-05-08 14:43:14 +0200

    Update main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,6 @@
 	<h1>Welcome!</h1>
 	<p>
 		Check out the Login form, to see the authentication workflow. It is possible to login using
-		email or github via. oauth. It is fully intigrated with password reset and email verification
+		email or github. It is fully intigrated with password reset and email verification
 	</p>
 </div>
