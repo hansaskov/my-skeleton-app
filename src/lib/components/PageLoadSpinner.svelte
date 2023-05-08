b047844fadc944e47a009c56761e35755e77f287
hansaskov <hansaskov1999@gmail.com>
2023-05-08 14:51:35 +0200

    add fade to page spinner

--- src/lib/components/PageLoadSpinner.svelte
+++ src/lib/components/PageLoadSpinner.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { beforeNavigate, afterNavigate } from '$app/navigation';
 	import { ProgressRadial } from '@skeletonlabs/skeleton';
+	import { fade } from 'svelte/transition';
 
 	// Only show spinner if page transition takes more than 100ms
 	const wait = (delay: number | undefined) => new Promise((res) => setTimeout(res, delay));
@@ -16,12 +17,14 @@
 
 {#if isLoading}
 	<!-- svelte-ignore empty-block -->
-	{#await wait(100) then a}
-		<ProgressRadial
-			stroke={125}
-			meter="stroke-primary-400"
-			track="stroke-primary-500/30"
-			width="w-8"
-		/>
+	{#await wait(150) then}
+		<div transition:fade>
+			<ProgressRadial
+				stroke={125}
+				meter="stroke-primary-400"
+				track="stroke-primary-500/30"
+				width="w-8"
+			/>
+		</div>
 	{/await}
 {/if}
