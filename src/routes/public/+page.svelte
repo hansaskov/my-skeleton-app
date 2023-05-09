afa51dfb82f33b4039f893d2acd75f24ca5fe751
hansaskov <hansaskov1999@gmail.com>
2023-05-09 13:16:12 +0200

    Show hidden data in public page

--- src/routes/public/+page.svelte
+++ src/routes/public/+page.svelte
@@ -1,10 +1,17 @@
-<script>
+<script lang="ts">
 	import Seo from '$lib/components/Seo.svelte';
+	import type { LayoutData } from '../$types';
+
+	export let data: LayoutData;
 </script>
 
 <Seo title="Askov | Public" type="WebPage" description="Showcase how a public site is accessable" />
 
 <div class="flex flex-col gap-8 items-center justify-center">
-	<h1>Wishlist overview!</h1>
-	<p>This page will showcase all family members with a link to their Wishlists</p>
+	<h1>Public page</h1>
+	<p>Everyone can view the contents of this page</p>
+	{#if data.user}
+		<p>Hidden data can only be seen when logged in</p>
+		{data.user.email}
+	{/if}
 </div>
