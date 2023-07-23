5179fea113b8a34e410301ae8b4bfab2fd5f77f3
hansaskov <hansaskov1999@gmail.com>
2023-07-23 22:35:43 +0200

    Add new empty page

---
+++ src/routes/family/+page.server.ts
@@ -0,0 +1,9 @@
+import type { PageServerLoad } from './$types';
+import { redirectFromPrivatePage } from '$lib/server/redirects/redirects';
+
+export const load: PageServerLoad = async (event) => {
+	const { user: authUser } = await event.locals.auth.validateUser();
+	const { userInfo, user } = await redirectFromPrivatePage(authUser, event);
+
+	return { userInfo, user };
+};
